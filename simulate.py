c76a49e4a1825e2a3f10431b3c084fb52a3161f3
ethanmbrett <ethanmbrett@gmail.com>
2024-01-21 14:44:54 -0500

    Made simulation
    Ease of recording change

--- simulate.py
+++ simulate.py
@@ -2,7 +2,7 @@ import pybullet as p
 import time as t
 physicsClient = p.connect(p.GUI)
 p.loadSDF("box.sdf")
-for i in range (0, 1000):
+for i in range (0, 2000):
     p.stepSimulation()
     t.sleep(0.016)
     print (i)
