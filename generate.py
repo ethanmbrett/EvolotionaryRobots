b69115809c75e59fb7453859cdcb89cf67ae654d
ethanmbrett <ethanmbrett@gmail.com>
2024-01-21 15:14:29 -0500

    Generated Towers

--- generate.py
+++ generate.py
@@ -1,5 +1,34 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("box.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[1,1,1])
+length = 1
+width = 1
+height = 1
+
+x = 0
+y = 0
+z = 0.5
+
+pyrosim.Start_SDF("boxes.sdf")
+
+hMod = 0
+lMod = 0
+wMod = 0
+sMod = 1.0
+
+for c in range (0, 5):
+    for r in range (0, 5):
+        for i in range (0, 10):
+            pyrosim.Send_Cube(name="Box"+str(c)+"-"+str(r)+"-"+str(i), 
+                              pos=[x+lMod,y+wMod,z+hMod] , 
+                              size=[length*sMod,width*sMod,height*sMod])
+            hMod = hMod + 1
+            sMod = sMod * 0.9
+        hMod = 0
+        sMod = 1.0
+        wMod = wMod + 1
+    wMod = 0
+    lMod = lMod + 1
+
+
+
 pyrosim.End()
--- simulate.py
+++ simulate.py
@@ -1,7 +1,12 @@
 import pybullet as p
 import time as t
+import pybullet_data
+
 physicsClient = p.connect(p.GUI)
-p.loadSDF("box.sdf")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+p.setGravity(0,0,-9.8)
+planeId = p.loadURDF("plane.urdf")
+p.loadSDF("boxes.sdf")
 for i in range (0, 2000):
     p.stepSimulation()
     t.sleep(0.016)
